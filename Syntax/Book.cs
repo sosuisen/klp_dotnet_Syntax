ae06c2ecd294278de093f8739efd702ca2b8ea30
sosuisen <hidekazu.kubota@gmail.com>
2024-02-13 00:36:07 +0900

    feat: add Book class

---
+++ Syntax/Book.cs
@@ -0,0 +1,16 @@
+namespace Syntax
+{
+    internal class Book
+    {
+        private string author;
+        private string title;
+        private int pages;
+
+        public Book(string author, string title, int pages)
+        {
+            this.author = author;
+            this.title = title;
+            this.pages = pages;
+        }
+    }
+}
