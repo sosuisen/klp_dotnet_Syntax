391cd654fe08aace4bc420951287a4e6b16b23e3
sosuisen <hidekazu.kubota@gmail.com>
2024-02-14 12:56:42 +0900

    fix: use order1

--- Syntax/Program.cs
+++ Syntax/Program.cs
@@ -4,10 +4,10 @@ namespace Syntax
     {
         static void Main(string[] args)
         {
-            Order order = new Order(1, "Alan Kay", DateTime.Now);
+            Order order1 = new Order(1, "Alan Kay", DateTime.Now);
             Order order2 = new Order(2, "Ted Nelson", DateTime.Now.AddDays(1));
 
-            order.DisplayOrder();
+            order1.DisplayOrder();
             order2.DisplayOrder();
         }
     }
