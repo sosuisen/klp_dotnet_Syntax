1de0ac3e1839553ff80de3af8e827db3d669598d
sosuisen <hidekazu.kubota@gmail.com>
2024-02-14 12:58:40 +0900

    fix: change date

--- Syntax/Program.cs
+++ Syntax/Program.cs
@@ -4,8 +4,8 @@ namespace Syntax
     {
         static void Main(string[] args)
         {
-            Order order1 = new Order(1, "Alan Kay", DateTime.Now);
-            Order order2 = new Order(2, "Ted Nelson", DateTime.Now.AddDays(1));
+            Order order1 = new Order(1, "Alan Kay", new DateTime(2022, 2, 2));
+            Order order2 = new Order(2, "Ted Nelson", new DateTime(2022, 3, 7));
 
             order1.DisplayOrder();
             order2.DisplayOrder();
